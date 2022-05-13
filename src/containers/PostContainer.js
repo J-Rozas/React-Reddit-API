65c13da6e2d4a95181e4fe6add6440bdc0f1eda1
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-13 22:41:47 +0100

    Add a post container

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const PostContainer = () => {
-  return "This is the post container";
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    postsFromAPI();
+  }, ([]))
+
+  const postsFromAPI = () => fetch('https://www.reddit.com/r/javascript.json')
+                        .then(response => response.json())
+                        .then(posts => setPosts(posts.data.children))
+
+
+
+  return (
+    <ul>
+      <li>Test</li>
+    </ul>
+  )
 }
 
 export default PostContainer;
